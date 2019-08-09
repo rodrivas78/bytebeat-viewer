2d3e2b7b03cb54ae0357177ce7fcbbca3ca63010
Rodrigo Gonzales <rrivas2009@gmail.com>
2019-08-09 02:41:24 -0300

    Update bytebeat_viewer.cpp

--- bytebeat_viewer.cpp
+++ bytebeat_viewer.cpp
@@ -1,5 +1,5 @@
 //*   
-//* Bytebeat Viewer  by  Rodrigo G. Rivas (rodrivas78)
+//* Bytebeat Viewer  by  Rodrigo G. (rodrivas78)
 //* This application generate visualizations for "bytebeats" ("one liners" or small programs
 //* that produce interesting sound and musical results). When plotted, many of 
 //* them result in fractals. So I thought it appropriate to call them "sound fractals" too. For more 
